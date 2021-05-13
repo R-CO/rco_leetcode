ba4a8a36cb723356b9f307fd71ecc504bfea9712
R-CO <daniel1820kobe@gmail.com>
2021-05-14 00:43:12 +0800

    [fix] Fix compile-error

--- src/easy/110_balanced_binary_tree.hpp
+++ src/easy/110_balanced_binary_tree.hpp
@@ -5,21 +5,23 @@
 #ifndef RCO_LEETCODE_SRC_EASY_110_BALANCED_BINARY_TREE_HPP
 #define RCO_LEETCODE_SRC_EASY_110_BALANCED_BINARY_TREE_HPP
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 class Solution {
  public:
-  bool isBalanced(TreeNode* root) {}
+  bool isBalanced(TreeNode *root) {
+    if (root == nullptr) { return false; }
+
+    return false;
+  }
 };
 
 #endif  // end of define RCO_LEETCODE_SRC_EASY_110_BALANCED_BINARY_TREE_HPP
