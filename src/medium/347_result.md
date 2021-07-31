580827192e6c802246e18da7af69dbf52cedfe36
R-CO <daniel1820kobe@gmail.com>
2021-08-01 01:15:55 +0800

    [perf] Problem 347 time improvement
    1. Use std::unordered_map to replace std::map.
    
    2. Use std::priority_queue to replace std::map.
    
    3. Pre-allocate enough memory for output.

--- src/medium/347_result.md
+++ src/medium/347_result.md
@@ -4,5 +4,5 @@
 
 ## Details
 
-Runtime: 20 ms, faster than 35.08% of C++ online submissions for Top K Frequent Elements.
-Memory Usage: 13.9 MB, less than 12.79% of C++ online submissions for Top K Frequent Elements.
+Runtime: 8 ms, faster than 99.35% of C++ online submissions for Top K Frequent Elements.
+Memory Usage: 13.7 MB, less than 39.31% of C++ online submissions for Top K Frequent Elements.
--- src/medium/347_top_k_frequent_elements.hpp
+++ src/medium/347_top_k_frequent_elements.hpp
@@ -5,33 +5,33 @@
 #ifndef RCO_LEETCODE_SRC_MEDIUM_347_TOP_K_FREQUENT_ELEMENTS_HPP
 #define RCO_LEETCODE_SRC_MEDIUM_347_TOP_K_FREQUENT_ELEMENTS_HPP
 
-#include <map>
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 namespace top_k_frequent_elements {
 class Solution {
  public:
   std::vector<int> topKFrequent(std::vector<int> &nums, int k) {
-    std::map<int, int> num_count;
+    if (k == nums.size()) {
+      return nums;
+    }
+
+    std::unordered_map<int, int> num_count;
     for (auto num : nums) {
       ++num_count[num];
     }
 
-    std::map<int, std::vector<int>> frequent_map;
-    for (auto num_count_pair : num_count) {
-      frequent_map[num_count[num_count_pair.first]].push_back(
-          num_count_pair.first);
+    std::priority_queue<std::pair<int, int>> frequent_queue;
+    for (const auto &num_count_pair : num_count) {
+      frequent_queue.push(
+          std::make_pair(num_count_pair.second, num_count_pair.first));
     }
 
-    std::vector<int> output;
-    auto it = frequent_map.rbegin();
-    size_t i = 0;
-    while (k-- > 0) {
-      output.push_back(it->second[i++]);
-      if (i == it->second.size()) {
-        i = 0;
-        ++it;
-      }
+    std::vector<int> output(static_cast<size_t>(k));
+    for (size_t i = 0; i < static_cast<size_t>(k); ++i) {
+      output[i] = frequent_queue.top().second;
+      frequent_queue.pop();
     }
 
     return output;
