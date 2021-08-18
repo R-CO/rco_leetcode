905a12eca4208a4c9ad27f76095ded7897e90f50
R-CO <daniel1820kobe@gmail.com>
2021-08-18 17:36:09 +0800

    [feat] Problem 1448 AC

---
+++ src/medium/1448_result.md
@@ -0,0 +1,8 @@
+# 1448 result
+
+## Success
+
+## Details
+
+Runtime: 116 ms, faster than 77.61% of C++ online submissions for Count Good Nodes in Binary Tree.
+Memory Usage: 86.3 MB, less than 53.33% of C++ online submissions for Count Good Nodes in Binary Tree.
--- xmake.lua
+++ xmake.lua
@@ -197,6 +197,14 @@ target("1170")
 	add_files("src/easy/1170_n_th_tribonacci_number_test.cpp")
 target_end()
 
+-- 1448. Count Good Nodes in Binary Tree
+target("1448")
+	set_kind("binary")
+
+	add_files("unit_test/unit_test_main.cpp")
+	add_files("src/medium/1448_count_good_nodes_in_binary_tree_test.cpp")
+target_end()
+
 -- 1701. Average Waiting Time
 target("1701")
 	set_kind("binary")
