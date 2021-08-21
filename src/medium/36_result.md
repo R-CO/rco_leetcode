afeb2a844cac34a0f9dc6ed4f25dd53e72bc340b
R-CO <daniel1820kobe@gmail.com>
2021-08-21 14:01:05 +0800

    [perf] Problem 36 time improvement
    1. Reduce some comparsion.
    
    2. 12 ms -> 4 ms

--- src/medium/36_result.md
+++ src/medium/36_result.md
@@ -4,5 +4,5 @@
 
 ## Details
 
-Runtime: 12 ms, faster than 98.38% of C++ online submissions for Valid Sudoku.
-Memory Usage: 17.9 MB, less than 88.44% of C++ online submissions for Valid Sudoku.
+Runtime: 4 ms, faster than 100.00% of C++ online submissions for Valid Sudoku.
+Memory Usage: 18.1 MB, less than 72.63% of C++ online submissions for Valid Sudoku.
--- src/medium/36_valid_sudoku.hpp
+++ src/medium/36_valid_sudoku.hpp
@@ -56,64 +56,103 @@ class Solution {
 
   bool checkSubBoxUsed(const size_t i, const size_t j, const size_t digit,
                        bool sub_box_used[][10]) {
-    if (i < 3 && j < 3) {
-      if (sub_box_used[0][digit]) {
-        return false;
-      }
+    if (i < 3) {
+      if (j < 3) {
+        if (sub_box_used[0][digit]) {
+          return false;
+        }
 
-      sub_box_used[0][digit] = true;
-    } else if (i < 3 && j >= 3 && j < 6) {
-      if (sub_box_used[1][digit]) {
-        return false;
+        sub_box_used[0][digit] = true;
+      } else if (j >= 3 && j < 6) {
+        if (sub_box_used[1][digit]) {
+          return false;
+        }
+        sub_box_used[1][digit] = true;
+      } else {
+        if (sub_box_used[2][digit]) {
+          return false;
+        }
+        sub_box_used[2][digit] = true;
       }
-
-      sub_box_used[1][digit] = true;
-    } else if (i < 3 && j >= 6 && j < 9) {
-      if (sub_box_used[2][digit]) {
-        return false;
+    } else if (i >= 3 && i < 6) {
+      if (j < 3) {
+        if (sub_box_used[3][digit]) {
+          return false;
+        }
+        sub_box_used[3][digit] = true;
+      } else if (j >= 3 && j < 6) {
+        if (sub_box_used[4][digit]) {
+          return false;
+        }
+        sub_box_used[4][digit] = true;
+      } else {
+        if (sub_box_used[5][digit]) {
+          return false;
+        }
+        sub_box_used[5][digit] = true;
       }
-
-      sub_box_used[2][digit] = true;
-    } else if (i >= 3 && i < 6 && j < 3) {
-      if (sub_box_used[3][digit]) {
-        return false;
+    } else {
+      if (j < 3) {
+        if (sub_box_used[6][digit]) {
+          return false;
+        }
+        sub_box_used[6][digit] = true;
+      } else if (j >= 3 && j < 6) {
+        if (sub_box_used[7][digit]) {
+          return false;
+        }
+        sub_box_used[7][digit] = true;
+      } else {
+        if (sub_box_used[8][digit]) {
+          return false;
+        }
+        sub_box_used[8][digit] = true;
       }
-
-      sub_box_used[3][digit] = true;
-    } else if (i >= 3 && i < 6 && j >= 3 && j < 6) {
-      if (sub_box_used[4][digit]) {
-        return false;
-      }
-
-      sub_box_used[4][digit] = true;
-    } else if (i >= 3 && i < 6 && j >= 6 && j < 9) {
-      if (sub_box_used[5][digit]) {
-        return false;
-      }
-
-      sub_box_used[5][digit] = true;
-    } else if (i >= 6 && i < 9 && j < 3) {
-      if (sub_box_used[6][digit]) {
-        return false;
-      }
-
-      sub_box_used[6][digit] = true;
-    } else if (i >= 6 && i < 9 && j >= 3 && j < 6) {
-      if (sub_box_used[7][digit]) {
-        return false;
-      }
-
-      sub_box_used[7][digit] = true;
-    } else if (i >= 6 && i < 9 && j >= 6 && j < 9) {
-      if (sub_box_used[8][digit]) {
-        return false;
-      }
-
-      sub_box_used[8][digit] = true;
     }
 
     return true;
   }
+
+#if 0
+  // smaller code size
+  bool checkSubBoxUsed(const size_t i, const size_t j, const size_t digit,
+                       bool sub_box_used[][10]) {
+    size_t sub_box_index = 0;
+
+    if (i < 3) {
+      if (j < 3) {
+        sub_box_index = 0;
+      } else if (j >= 3 && j < 6) {
+        sub_box_index = 1;
+      } else {
+        sub_box_index = 2;
+      }
+    } else if (i >= 3 && i < 6) {
+      if (j < 3) {
+        sub_box_index = 3;
+      } else if (j >= 3 && j < 6) {
+        sub_box_index = 4;
+      } else {
+        sub_box_index = 5;
+      }
+    } else {
+      if (j < 3) {
+        sub_box_index = 6;
+      } else if (j >= 3 && j < 6) {
+        sub_box_index = 7;
+      } else {
+        sub_box_index = 8;
+      }
+    }
+
+    if (sub_box_used[sub_box_index][digit]) {
+      return false;
+    }
+
+    sub_box_used[sub_box_index][digit] = true;
+    return true;
+  }
+#endif
 };
 
 }  // end of namespace valid_sudoku
